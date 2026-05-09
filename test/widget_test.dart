18eec1e9e496f145bc3b12372b748b8eff91e4e4
Bryson Villago <bvil536@gmail.com>
2026-05-09 13:18:03 -0700

    updated main

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:cutsmart/main.dart';
+import 'package:cutsmart/landing_page.dart';
 
 void main() {
   testWidgets('Profile page shows camera scanner button', (
